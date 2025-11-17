6be9444dd667bbcbe4629663ca089204cfc7bdd1
tiennv67khmt-dev <tien.nv.67khmt@ntu.edu.vn>
2025-11-17 10:12:24 +0700

    Create Kiem_tra_ngay_trong_tuan.py

---
+++ Kiem_tra_ngay_trong_tuan.py
@@ -0,0 +1,18 @@
+print('nhập một số nguyên từ 2 đến 8: ',end='')
+n = int(input())
+if n < 2 or n > 8:
+    print('số không hợp lệ')
+if n==2:
+    print('Thứ 2')
+elif n==3:
+    print('Thứ 3')
+elif n==4:
+    print('Thứ 4')
+elif n==5:
+    print('Thứ 5')
+elif n==6:
+    print('Thứ 6')
+elif n==7:
+    print('Thứ 7')
+else:
+    print('Chủ nhật')
