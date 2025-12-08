06ba7fb7c9ee369b7202781fcdab10c0ce61ac2d
tiennv67khmt-dev <tien.nv.67khmt@ntu.edu.vn>
2025-12-08 14:05:04 +0700

    Update bai_test.py

--- bai_test.py
+++ bai_test.py
@@ -1,5 +1,14 @@
+# viết số trung bình cộng của 3 số nguyên a, b, c
 a = int(input('nhập số a: '))
 b = int(input('nhập số b: '))
 c = int(input('nhập số c: '))
 trung_binh = (a + b + c) / 3
 print(f'Trung bình cộng của {a}, {b}, {c} là: {trung_binh}')
+
+
+# viết chương trình nhập vào số nguyên, cho biết số đó có đồng thời chia hết cho 3 và 5 hay không ?
+n = int(input('nhập vào một số: '))
+if n % 3 == 0 and n % 5 == 0:
+    print('chia hết cho 3 và chia hết cho 5')
+else: 
+    print('không chia hết cho 3 và 5')
