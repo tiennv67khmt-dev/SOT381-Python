f537e3ca9db02d97802fc6a3ca8c71c66001d733
tiennv67khmt-dev <tien.nv.67khmt@ntu.edu.vn>
2025-12-08 13:46:56 +0700

    Create bai_test.py

---
+++ bai_test.py
@@ -0,0 +1,5 @@
+a = int(input('nhập số a: '))
+b = int(input('nhập số b: '))
+c = int(input('nhập số c: '))
+trung_binh = (a + b + c) / 3
+print(f'Trung bình cộng của {a}, {b}, {c} là: {trung_binh}')
