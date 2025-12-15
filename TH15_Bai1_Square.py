904293441fab6f1918cd389e5a4ed524edd27e08
tiennv67khmt-dev <tien.nv.67khmt@ntu.edu.vn>
2025-12-15 13:54:54 +0700

    Create TH15_Bai1_Square.py

---
+++ TH15_Bai1_Square.py
@@ -0,0 +1,6 @@
+a = int(input("Nhập chiều dài (cm): "))
+b = int(input("Nhập chiều rộng (cm): "))
+chu_vi = (a + b) * 2
+dien_tich = a * b
+print(f"Chu vi hình chữ nhật là: {chu_vi} cm")
+print(f"Diện tích hình chữ nhật là: {dien_tich} cm",)
