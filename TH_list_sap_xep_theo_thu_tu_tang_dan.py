4457790e77be47342e74c1a88c846607f93749ad
tiennv67khmt-dev <tien.nv.67khmt@ntu.edu.vn>
2025-12-21 21:59:17 +0700

    Delete TH_list_sap_xep_theo_thu_tu_tang_dan.py

--- TH_list_sap_xep_theo_thu_tu_tang_dan.py
+++
@@ -1,8 +0,0 @@
-n = int(input("Nhập số phần tử của danh sách: "))
-ds = []
-for i in range(n):
-    a = input(f'nhập phần tử thứ {i}: ')
-    ds.append(a)
-print(f'danh sách ban đầu: {ds}')
-ds.sort()
-print(f'danh sách sau khi sắp xếp: {ds}')
